70f4049f1a798be741087f5ba762571a5ec3379c
Markus Hitter <mah@jump-ing.de>
2018-07-25 19:19:02 +0200

    Introduce AdminCoreUpdaterController.
    So far no content display.
    
    This commit served as the example to write
    http://jump-ing.com/traumflugs/back_office_menu_entries,
    section 'Creating a Controller'.

---
+++ controllers/admin/AdminCoreUpdaterController.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Copyright (C) 2018 thirty bees
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@thirtybees.com so we can send you a copy immediately.
+ *
+ * @author    thirty bees <modules@thirtybees.com>
+ * @copyright 2018 thirty bees
+ * @license   Academic Free License (AFL 3.0)
+ */
+
+if (!defined('_TB_VERSION_')) {
+    exit;
+}
+
+/**
+ * Class AdminCoreUpdaterController.
+ */
+class AdminCoreUpdaterController extends ModuleAdminController
+{
+    /**
+     * AdminCoreUpdaterController constructor.
+     *
+     * @since 1.0.0
+     */
+    public function __construct()
+    {
+        $this->bootstrap = true;
+
+        parent::__construct();
+    }
+
+    /**
+     * Get back office page HTML.
+     *
+     * @return string Page HTML.
+     *
+     * @since 1.0.0
+     */
+    public function initContent()
+    {
+        $this->page_header_toolbar_title = $this->l('Core Updater');
+
+        parent::initContent();
+    }
+}
