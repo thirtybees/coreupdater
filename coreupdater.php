0b122504711a680810525341f280846ffb3253b2
Markus Hitter <mah@jump-ing.de>
2019-02-05 21:00:09 +0100

    Make release 1.0.0.

--- coreupdater.php
+++ coreupdater.php
@@ -41,7 +41,7 @@ class CoreUpdater extends Module
     {
         $this->name = 'coreupdater';
         $this->tab = 'administration';
-        $this->version = '0.9.0';
+        $this->version = '1.0.0';
         $this->author = 'thirty bees';
         $this->bootstrap = true;
         $this->need_instance = 0;
