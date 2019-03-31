aec1cebdc77d9d9b418d2dcdd0f2e54a9c32327e
Petr Hučík <petr@getdatakick.com>
2019-03-31 18:33:00 +0200

    Introduce SchemaDifference interface
    This interface represent some difference between two database schemas,
    for example missing table, different colum type, different column
    default value, etc.
    
    At the moment the interface contains only method to report on these
    differences. Plans are to extend it with methods to migrate database
    and rectify the situation.
    
    That's also the reason why there will be many implementation classes,
    each of them will know how to fix specific situation.
    
    Also, some changes will be considiered safe, and can be applied
    automatically (missing database colum, for instance). Other changes
    will require user assistance / approval (dropping colums, changing
    column type in a destructive fashion,...)

---
+++ classes/schema/SchemaDifference.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Copyright (C) 2019 thirty bees
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@thirtybees.com so we can send you a copy immediately.
+ *
+ * @author    thirty bees <contact@thirtybees.com>
+ * @copyright 2019 thirty bees
+ * @license   Open Software License (OSL 3.0)
+ */
+
+namespace CoreUpdater;
+
+if (!defined('_TB_VERSION_')) {
+    exit;
+}
+/**
+ * Interface SchemaDifference
+ *
+ * Interface to represents difference between two database schemas
+ *
+ * At the moment, this interface only allows for reporting on database difference. In the future there will be another
+ * methods to rectify the situation (apply database migration changes)
+ *
+ * @since 1.1.0
+ */
+interface SchemaDifference
+{
+    function describe();
+}
