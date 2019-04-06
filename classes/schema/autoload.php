b51526778ebacdc0c4761b1a37665277e09c3908
Markus Hitter <mah@jump-ing.de>
2019-04-06 16:39:48 +0200

    classes/schema/*: adjust license for use in the module.
    Code in classes/schema was initially developed for thirty bees
    core, so it had the OSL license. Modules use the AFL license.

--- classes/schema/autoload.php
+++ classes/schema/autoload.php
@@ -4,17 +4,17 @@
  *
  * NOTICE OF LICENSE
  *
- * This source file is subject to the Open Software License (OSL 3.0)
+ * This source file is subject to the Academic Free License (AFL 3.0)
  * that is bundled with this package in the file LICENSE.md.
  * It is also available through the world-wide-web at this URL:
- * https://opensource.org/licenses/osl-3.0.php
+ * https://opensource.org/licenses/afl-3.0.php
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to license@thirtybees.com so we can send you a copy immediately.
  *
- * @author    thirty bees <contact@thirtybees.com>
+ * @author    thirty bees <modules@thirtybees.com>
  * @copyright 2019 thirty bees
- * @license   Open Software License (OSL 3.0)
+ * @license   Academic Free License (AFL 3.0)
  */
 
 require_once __DIR__.'/DatabaseCharset.php';
