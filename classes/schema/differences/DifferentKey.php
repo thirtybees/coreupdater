343c22dd238ec8e4f721d54c5df435358af2cab1
Petr Hučík <petr@getdatakick.com>
2019-07-08 12:10:35 +0200

    DifferentKey: implement migration

--- classes/schema/differences/DifferentKey.php
+++ classes/schema/differences/DifferentKey.php
@@ -18,7 +18,9 @@
  */
 
 namespace CoreUpdater;
+
 use \Translate;
+use \Db;
 
 if (!defined('_TB_VERSION_')) {
     exit;
@@ -79,4 +81,49 @@ class DifferentKey implements SchemaDifference
             $this->table->getName()
         );
     }
+
+    /**
+     * Returns unique identification of this database difference.
+     *
+     * @return string
+     */
+    function getUniqueId()
+    {
+        return get_class($this) . ':' . $this->table->getName() . '.' . $this->key->getName();
+    }
+
+    /**
+     * This operation is NOT destructive
+     *
+     * @return bool
+     */
+    function isDestructive()
+    {
+        return false;
+    }
+
+    /**
+     * Returns severity of this difference
+     *
+     * @return int severity
+     */
+    function getSeverity()
+    {
+        return self::SEVERITY_NORMAL;
+    }
+
+    /**
+     * Applies fix to correct this database difference
+     *
+     * @param Db $connection
+     * @return bool
+     * @throws \PrestaShopException
+     */
+    function applyFix(Db $connection)
+    {
+        $stmt = 'ALTER TABLE `' . bqSQL($this->table->getName()) . '` DROP KEY `' . bqSQL($this->key->getName()) . '`, ADD ' .$this->key->getDDLStatement();
+        return $connection->execute($stmt);
+    }
+
 }
+
