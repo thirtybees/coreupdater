d6c824f73fd12a61258bd81fe4259c066e93be17
Petr Hučík <petr@getdatakick.com>
2021-03-31 12:35:52 +0200

    Release version 1.3.3

--- coreupdater.php
+++ coreupdater.php
@@ -41,7 +41,7 @@ class CoreUpdater extends Module
     {
         $this->name = 'coreupdater';
         $this->tab = 'administration';
-        $this->version = '1.3.2';
+        $this->version = '1.3.3';
         $this->author = 'thirty bees';
         $this->bootstrap = true;
         $this->need_instance = 0;
