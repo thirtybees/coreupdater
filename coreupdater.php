c5786765af8daf7e74c588cba70ab8950fc98b90
Petr Hučík <petr@getdatakick.com>
2022-10-18 09:56:44 +0200

    Release coreupdater v1.6.1

--- coreupdater.php
+++ coreupdater.php
@@ -38,7 +38,7 @@ class CoreUpdater extends Module
     {
         $this->name = 'coreupdater';
         $this->tab = 'administration';
-        $this->version = '1.6.0';
+        $this->version = '1.6.1';
         $this->author = 'thirty bees';
         $this->bootstrap = true;
         $this->need_instance = 0;
